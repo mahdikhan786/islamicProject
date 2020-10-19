98310cbb6d3f06ca7802b216c2882b6143e5fc6e
mahdi khan #khansofficial <khanmahdi786@gmail.com>
2020-10-18 22:36:52 -0700

    timer is fun !

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,37 @@ import styles from '../styles/index.module.css'
 
 export default function Home({ language }) {
   const [appLang, setAppLang] = useState("lang");
+  const [timeLeft,setTimeLeft] = useState('');
+  const [done, setDone] = useState(false)
   useEffect(() => {
     if (language) {
       setAppLang(language);
+      var countDownDate = new Date("Oct 20, 2020 10:30:00").getTime();
+      var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    setTimeLeft(`${hours} hours ${minutes} mins ${seconds} seconds `)
+  // // Output the result in an element with id="demo"
+  // document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  // + minutes + "m " + seconds + "s ";
+  //
+  // If the count down is over, write some text
+  if (distance < 0) {
+    clearInterval(x);
+  setDone(true)
+  }
+}, 1000);
     }
   });
   return (
@@ -23,7 +51,10 @@ export default function Home({ language }) {
 
           GOOD LUCK WITH THE HACK ABBAS AND JaWAD !
           <br/>
-          You've got 24 hours ! tick tick tick ...
+          You've got {timeLeft} left !
+<br />
+           tick tick tick ...
+           {done ? 'YOU HAVE FAILED YOUR MISSION !' : ""}
         </h1>
       </main>
     </Layout>
